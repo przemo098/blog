ad2875fc963c22ed6ab6700edeea4fd88fa7351b
przemo098 <przemo098@hotmail.com>
2019-11-25 21:57:08 +0100

    add journeys coponent

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import UrlPaths from "./routing/UrlPaths";
 
 @Component({
   selector: "app-root",
@@ -6,6 +7,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+  journeys = UrlPaths.journeys;
   title = "app";
   isNavMenu: boolean = false;
   navMinWidth: string;
--- blog/src/app/routing/routing.ts
+++ blog/src/app/routing/routing.ts
@@ -2,7 +2,7 @@ import { Routes } from "@angular/router";
 import { AboutComponent } from "../pages/about/about.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { WelcomePageComponent } from "../pages/welcome-page/welcome-page.component";
-import UrlPaths from "./routes";
+import UrlPaths from "./UrlPaths";
 import { JourneyComponent } from "../pages/journeys/journeys.component";
 
 export const appRoutes: Routes = [
@@ -20,14 +20,14 @@ export const appRoutes: Routes = [
     path: "",
     component: WelcomePageComponent,
     data: { title: "About Kaminsky" }
-  },  
-  {
-    path: "**",
-    component: PageNotFoundComponent
-  },  
+  },
   {
     path: UrlPaths.journeys,
     component: JourneyComponent,
     data: { title: UrlPaths.journeys }
+  },  
+  {
+    path: "**",
+    component: PageNotFoundComponent
   }
 ];
