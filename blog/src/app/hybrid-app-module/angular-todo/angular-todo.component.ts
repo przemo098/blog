218ae21fa109d18ce1ae15f787b5baccfb594298
przemo098 <przemo098@hotmail.com>
2020-04-08 22:33:22 +0200

    angular works without react

--- blog/src/app/hybrid-app-module/angular-todo/angular-todo.component.ts
+++ blog/src/app/hybrid-app-module/angular-todo/angular-todo.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-// import AppState, { TodoItem } from 'shared/src'
+import { Component, OnInit, AfterContentInit } from "@angular/core";
+import AppState, { TodoItem } from '@przemo098/shared'
 
 @Component({
   selector: "app-angular-todo",
   templateUrl: "./angular-todo.component.html",
   styleUrls: ["./angular-todo.component.scss"]
 })
-export class AngularTodoComponent implements OnInit {
-  // items: TodoItem[];
-  // newItem: TodoItem;
+export class AngularTodoComponent implements AfterContentInit {
+  items: TodoItem[];
+  newItem: TodoItem;
   constructor() {
-    // this.items = AppState.items.value;
-    // this.newItem = AppState.newItem.value;
+    this.items = AppState.items.value;
+    this.newItem = AppState.newItem.value;
+  }
+  ngAfterContentInit(): void {
+    AppState.newItem.subscribe(x => this.newItem = x);
+    AppState.items.subscribe(x => this.items = x);
   }
 
   onKeyDown() {
-    // AppState.newItem.next(new TodoItem(this.newItem.text));
+    AppState.newItem.next(new TodoItem(this.newItem.text));
   }
 
   addItem() {
-    // AppState.items.next(AppState.items.value.concat(AppState.newItem.value));
-    // AppState.newItem.next(new TodoItem());
+    AppState.items.next(AppState.items.value.concat(AppState.newItem.value));
+    AppState.newItem.next(new TodoItem());
   }
 
-  // removeItem(item: TodoItem) {
-  //   AppState.items.next(AppState.items.value.filter(x => x !== item));
-  // }
-
-  ngOnInit() {
+  removeItem(item: TodoItem) {
+    AppState.items.next(AppState.items.value.filter(x => x !== item));
   }
 }
