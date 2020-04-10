728856f378b084a424099cfa47744d1333caa0d3
przemo098 <przemo098@hotmail.com>
2020-04-10 18:54:59 +0200

    fix shared library

--- shared/src/stateManager.ts
+++ shared/src/stateManager.ts
@@ -1,17 +1,19 @@
 import AppState, { TodoItem } from "./todoState";
 
 export class TodoStateManager {
-    addNewElement(item: TodoItem) {
-        AppState.items.next([...AppState.items.value, item]);
+    private state = AppState;
+    addNewElement() {
+        this.state.items.next([...this.state.items.value, this.state.newItem.value]);
+        this.state.newItem.next(new TodoItem());
     }
 
     deleteItem(key: number) {
-        const filteredItems = AppState.items.value.filter(x => x.key !== key);
-        AppState.items.next(filteredItems);
+        const filteredItems = this.state.items.value.filter(x => x.key !== key);
+        this.state.items.next(filteredItems);
     }
 
     updateNewTodo(item: string) {
         const newValue = new TodoItem(item);
-        AppState.newItem.next(newValue);
+        this.state.newItem.next(newValue);
     }
 }
